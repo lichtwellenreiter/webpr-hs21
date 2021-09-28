28b61d743b86f0c69cedb67f76f2e6ac82891e75
Florian Thiévent <florianjeremias@gmail.com>
2021-09-28 21:02:47 +0200

    added homework

--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -1,11 +1,29 @@
-
 const radius = 10;
-const ball = {x:20, y:0, dx: 5, dy: 1};
-let   old  = {x: ball.x, y: ball.y};
+const ball = {x: 20, y: 0, dx: 5, dy: 1};
+let old = {x: ball.x, y: ball.y};
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let velocity = 1;
+let inertia = 0.9;
+
+
+function startOverRandom(x = 0) {
+    (x === 0) ? ball.x = Math.random() * (canvas.width - radius) + radius : ball.x = x;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    ball.y = 0
+    ball.dx = 5
+    ball.dy = 1
+
+}
+
+function startOver() {
+    startOverRandom(20)
+}
+
 
 function start() {
-    const canvas  = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
     context.fillStyle = "black";
 
     setInterval(() => {
@@ -15,7 +33,11 @@ function start() {
 }
 
 function nextBoard() {
+
     // keep old ball values for the sake of efficient clearing of the old display
+    old.x = ball.x
+    old.y = ball.y
+
 
     // handle ball is hitting the bounds
     //   reverse direction
@@ -23,12 +45,28 @@ function nextBoard() {
 
     // calculate new position
     // calculate any changes in velocity due to gravitational pull or medium resistance
+    if (ball.y + radius > canvas.height) {
+        ball.dy = -ball.dy * inertia
+    } else {
+        ball.dy += velocity
+    }
+
+    if (ball.x + radius > canvas.width || ball.x - radius < 0) {
+        ball.dx = -(ball.dx)
+    }
+
+    ball.y += ball.dy;
+    ball.x += ball.dx;
+
+    if (Math.floor(ball.y) === Math.floor(old.y)) {
+        ball.dx = ball.dx * 0.98
+    }
 
 
 }
 
 function display(context) {
-    context.clearRect(old.x - radius - 1 , old.y - radius -1 , 22, 22 );
+    context.clearRect(old.x - radius - 1, old.y - radius - 1, 22, 22);
     fillBox(context)
 }
 
