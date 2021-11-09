d37045e1ebb7440efb7100ef532e692f67c7c423
Florian Thiévent <florianjeremias@gmail.com>
2021-11-09 17:47:46 +0100

    week8

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -1,8 +1,21 @@
+let currentTodoIndex = 0;
+
 
 function startTodo() {
 
 }
 
 function addTodo() {
-    
+    const container = document.getElementById("todoContainer");
+
+    container.innerHTML += `
+    <tr>
+        <td>
+            <label for="todo${currentTodoIndex}" >My first todo${currentTodoIndex}</label>
+            <input type="text"  name="todo${currentTodoIndex}" 
+                                id="todo${currentTodoIndex}" 
+                                value="My first todo${currentTodoIndex}">
+        </td>
+    </tr>`;
+    currentTodoIndex++;
 }
