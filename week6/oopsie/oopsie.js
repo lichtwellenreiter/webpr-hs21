65a645dad91ec75311d2d8561591d6795ea0ff23
Florian Thiévent <florianjeremias@gmail.com>
2021-11-09 15:24:37 +0100

    week7

--- week6/oopsie/oopsie.js
+++ week6/oopsie/oopsie.js
@@ -8,14 +8,29 @@
 //   proceed(stride) // proceed so many places
 //   fallback()      // "oopsie": go back to last start (fallback position)
 //   turn()          // cash in your win, update fallback position for next turn
-// 
+//
+
+
+const Player = n => {
+    let name = n;
+    let fallbackIndex = 0;
+    let progressIndex = 0;
+
+    return {
+        proceed : stride =>  progressIndex += stride ,
+        fallback: () => progressIndex = fallbackIndex,
+        turn: () => fallbackIndex = progressIndex,
+        getFallbackIndex: () => fallbackIndex,
+        getProgressIndex: () => progressIndex
+    }
+}
 
 function start() {
     const fields = document.getElementById('fields');
 
     for (let i = 0; i < 100; i++) {
         let field = document.createElement("DIV");
-        field.setAttribute("ID", "FIELD-"+i);
+        field.setAttribute("ID", "FIELD-" + i);
         field.innerText = " ";
         fields.appendChild(field);
     }
@@ -24,7 +39,7 @@ function start() {
 
 function dice() {
     let stride = Math.round(1 + Math.random() * 5);
-    document.getElementById('dice').innerText = ""+ stride;
+    document.getElementById('dice').innerText = "" + stride;
     if (stride === 3) {
         player.fallback();
     } else {
@@ -40,13 +55,15 @@ function turn() {
 
 function display() {
     for (let i = 0; i < 100; i++) {
-        let field = document.getElementById("FIELD-"+i);
+        let field = document.getElementById("FIELD-" + i);
         field.setAttribute("CLASS", "field");
     }
-    let fallbackfield = document.getElementById("FIELD-"+ player.getFallbackIndex());
+    let fallbackfield = document.getElementById("FIELD-" + player.getFallbackIndex());
     fallbackfield.setAttribute("CLASS", "field fallback");
-    let progressfield = document.getElementById("FIELD-"+ player.getProgressIndex());
+    let progressfield = document.getElementById("FIELD-" + player.getProgressIndex());
     progressfield.setAttribute("CLASS", "field progress");
 }
+
+
 
 player = Player("One");
