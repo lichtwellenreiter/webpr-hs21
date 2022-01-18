7c325e4826982267b45a10df42a37ee6d87f74c3
Florian Thiévent <hi@thievent.org>
2022-01-18 15:32:59 +0100

    reduce week 14

--- week14/src/refresherTest.js
+++ week14/src/refresherTest.js
@@ -1,4 +1,5 @@
 import {TestSuite} from "./kolibri/util/test.js";
+import "./kolibri/util/array.js";
 
 const testSuite = TestSuite("refresher");
 
@@ -13,6 +14,32 @@ testSuite.add("reduce", assert => {
     assert.is(join(names), "Florian Katrin Niels");
 
     assert.is(join([]), "");
+
+});
+
+testSuite.add("splice", assert => {
+    let names = ["Florian", "Katrin", "Niels"];
+    names.splice(0, 1)
+    assert.isTrue(names.eq(["Katrin", "Niels"]));
+
+    names = ["Florian", "Katrin", "Niels"];
+    names.splice(0, 0)
+    assert.isTrue(names.eq(names));
+
+    names = ["Florian", "Katrin", "Niels"];
+    names.splice(0, 0, "Aaron", "Manuel")
+    assert.isTrue(names.eq(["Aaron", "Manuel", "Florian", "Katrin", "Niels"]));
+});
+
+testSuite.add("callback", assert => {
+
+    const benchmark = callback => {
+        const start = performance.now();
+        callback();
+        const end = performance.now();
+        return end - start;
+    };
+    assert.isTrue(benchmark(() => console.log("hi")) > 0);
 
 });
 
